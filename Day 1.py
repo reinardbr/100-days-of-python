81aaecd36c697b5103dfb20293c3dc528de53786
reinardbr <52466345+reinardbr@users.noreply.github.com>
2024-10-05 00:17:17 -0300

    Add files via upload
    Create a name generator based in inputs from the user.

---
+++ Day 1.py
@@ -0,0 +1,6 @@
+#Day 1 Challenge
+#create a Band Name Generator
+print("Welcome to the Company Name Generator.")
+founder_name = input("Whats is your last name?")
+partner_name = input("Whats is your partner last name?")
+print("Your company name could be " + founder_name + " " + partner_name)
